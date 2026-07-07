7241de8e4e2017a2b36021747b5ac0de6c35ce49
0x1mr0uss <abdelyassin162@gmail.com>
2026-07-07 19:20:20 +0000

    Day 1 : adding input user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
 fn main(){
-
+let mut user_input: String = String::new();
+io::stdin()
+    .read_line(&mut user_input)
+    .expect("Failed to read line");
+println!("{}", user_input.trim());
 }
