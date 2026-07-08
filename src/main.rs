5ca8a3f466b2bf941793abc41c02efbe48df84ad
0x1mr0uss <abdelyassin162@gmail.com>
2026-07-08 23:35:41 +0000

    day 2: (feat)implement user greeting using whoami and handle dynamic stdin input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 fn main(){
-let user_name : String = whoami::username();
+let user_name : String = whoami::username(); // by the way, this is a crate that can be used to get the username of the current user
 let mut user_input: String = String::new();
 println!("how can i help you, {} today?", user_name);
 io::stdin()
