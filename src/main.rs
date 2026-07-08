8153df369a36f84d38c12a883200276abb76f090
0x1mr0uss <abdelyassin162@gmail.com>
2026-07-08 23:19:30 +0000

    day 2: (feat)implement user greeting using whoami and handle dynamic stdin input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::io;
 fn main(){
+let user_name : String = whoami::username();
 let mut user_input: String = String::new();
+println!("how can i help you, {} today?", user_name);
 io::stdin()
     .read_line(&mut user_input)
     .expect("Failed to read line");
-println!("{}", user_input.trim());
+println!("if you, {} i am here to help!", user_input.trim());
 }
