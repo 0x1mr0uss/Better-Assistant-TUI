274f020d066e719c5a875abb93dae116275ab229
0x1mr0uss <abdelyassin162@gmail.com>
2026-07-09 16:42:33 +0000

    (feat) adding page for about program

---
+++ src/about.rs
@@ -0,0 +1,4 @@
+pub fn about_better(user_name: &str) ->  &str {
+    println!("hello {user_name}, i am better your assistant TUI of version \n v0.1.0-(Beta).");
+    user_name
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
+mod about;
 use std::io;
 use local_ip_address::local_ip;
 fn main(){
 let user_name : String = whoami::username(); // by the way, this is a crate that can be used to get the username of the current user
-let mut user_ip = local_ip().unwrap_or_else(|_| std::net::IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1)));
 let mut user_input: String = String::new();
+
 println!("how can i help you, {} today?", user_name);
-io::stdin()
-    .read_line(&mut user_input)
-    .expect("Failed to read line");
-if user_input.trim() == "Ip" {
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
+let trimmed_input = user_input.trim().to_lowercase();
+
+if trimmed_input == "ip" {
+    let user_ip = local_ip().unwrap_or_else(|_| std::net::IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1)));
     println!(" your ip is {user_ip} sir {user_name}");
-};
+}else if trimmed_input == "about" {
+    about::about_better(&user_name);
+}else {
+    println!("sorry, {user_name} that's Out of my service.")
+}
 }
