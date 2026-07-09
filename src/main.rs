321c4e0b02e682c47a386365bb99d01a3f897ff2
0x1mr0uss <abdelyassin162@gmail.com>
2026-07-09 16:43:41 +0000

    (feat) adding page for about program

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod about;
 use std::io;
 use local_ip_address::local_ip;
 fn main(){
-let user_name : String = whoami::username(); // by the way, this is a crate that can be used to get the username of the current user
+let user_name : String = whoami::username();  // by the way, this is a crate that can be used to get the username of the current user
 let mut user_input: String = String::new();
 
 println!("how can i help you, {} today?", user_name);
