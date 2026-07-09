f915fc45833cca50397ddfa98f4fbb4d9621f12a
0x1mr0uss <abdelyassin162@gmail.com>
2026-07-09 01:13:39 +0000

    day 3: (feat)adding local ip adress to get user device IP.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::io;
+use local_ip_address::local_ip;
 fn main(){
 let user_name : String = whoami::username(); // by the way, this is a crate that can be used to get the username of the current user
+let mut user_ip = local_ip().unwrap_or_else(|_| std::net::IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1)));
 let mut user_input: String = String::new();
 println!("how can i help you, {} today?", user_name);
 io::stdin()
     .read_line(&mut user_input)
     .expect("Failed to read line");
-println!("if you, {} i am here to help!", user_input.trim());
+if user_input.trim() == "Ip" {
+    println!(" your ip is {user_ip} sir {user_name}");
+};
 }
